c6237ddf7218d2247bb2f94f1c92159a86e4fa34
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2023-03-09 11:00:02 +0100

    install and configure Jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bernard-geraud';
+
+  ngOnInit(): void {
+    // $(document).ready(() => {
+    //   const headerHeight = $('app-header').css('height');
+    //   console.log(headerHeight);
+    // });
+  }
 }
