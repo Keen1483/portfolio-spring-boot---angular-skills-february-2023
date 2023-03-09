122912b73dc248238679353e52d26fe89fdd9c87
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2023-03-09 10:15:31 +0100

    Creating routing links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ExperiencesComponent } from './component/experiences/experiences.component';
+import { WorksComponent } from './component/works/works.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FourOhFourComponent } from './component/four-oh-four/four-oh-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'experiences', component: ExperiencesComponent},
+  {path: 'works', component: WorksComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: FourOhFourComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
